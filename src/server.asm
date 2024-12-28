e8d90880b5697e195656dcb70e6147e9fdab1394
Tathya Garg <tathya.garg@gmail.com>
2024-12-28 18:10:15 +0530

    Close socket + create socket address

--- src/server.asm
+++ src/server.asm
@@ -1,8 +1,45 @@
 %include 'src/functions.asm'
+%include 'src/sockets.asm'
+
+section .data
+    socket      dq      0
+    address     dq      0100007Fh        ; 127.0.0.1 in big endian hex
+    port        dw      901Fh            ; 8080 in little endian hex
+
+section .bss
+    socket_addr resq    1  ; Declare 8 bytes of uninitialized memory
 
 section .text
 global _start
 
 _start:
+    call create_socket
+    mov  [socket], rax                 ; Store socket file descriptor
+
+    ; ========== Create Socket Address ==========
+    ; The socket address structure is defined in netinet/in.h
+    ; struct sockaddr_in {
+    ;     sa_family_t    sin_family;  // Address Family
+    ;     in_port_t      sin_port;    // Port Number
+    ;     struct in_addr sin_addr;    // Internet Address
+    ; };
+    ;
+    ; sin_family is the address family (AF_INET). Size: word (2 bytes)
+    ; sin_port is the port number. Size: word (2 bytes)
+    ; sin_addr is the internet address. Size: dword (4 bytes)
+    ;
+    ; When elements are pushed to the stack, rsp is decremented.
+    ; This means to restore rsp to its original value, we need to add to it (not subtract!)
+    ; 
+    push rbp                             ; Store stack pointer (base)
+    mov  rbp,  rsp                       ; Move stack top to base
+    push qword [address]                 ; Push the address to the stack
+    push word  [port]                    ; Push the port to the stack
+    push word  2                         ; Push the address family (AF_INET) to the stack
+    mov  [socket_addr], rsp              ; Store the address pointer
+    add  rsp, 8                          ; Add to the stack pointer to restore its position 
+    pop  rbp                             ; Restore original stack pointer
+
+    call close_socket
     call quit
 
--- src/sockets.asm
+++ src/sockets.asm
@@ -1,4 +1,4 @@
-; ======= Creates a socket =======
+; ======= Create Socket =======
 ; The socket creation function, from sys/socket.h is:
 ; int socket(int domain, int type, int protocol)
 ;     Source: https://pubs.opengroup.org/onlinepubs/009619199/socket.htm
@@ -10,9 +10,28 @@
 ; 
 ; Returns: socket file descriptor
 create_socket:
-    mov     rax,41                  ; sys_socket()
-    mov     rdi,2                   ; AF_INET
-    mov     rsi,1                   ; SOCK_STREAM
-    mov     rdx,0                   ; Protocol
+    mov     rax, 41                  ; sys_socket()
+    mov     rdi, 2                   ; AF_INET
+    mov     rsi, 1                   ; SOCK_STREAM
+    mov     rdx, 0                   ; Protocol
+    syscall
+    ret
+
+; ======= Close Socket =======
+; We can use sys_close() to close the socket, which is opcode 3
+; Instead, we will use sys_shutdown() to gracefully close the socket
+; int shutdown(int socket, int how)
+;     Source: https://pubs.opengroup.org/onlinepubs/009695399/functions/shutdown.html
+;
+; The opcode for sys_shutdown() is 48
+; socket: socket file descriptor
+; how: 2 (SHUT_RDWR) - Disables further send and receive operations
+; 
+; Returns: 0 on success, -1 on error
+close_socket:
+    push rax
+    mov     rax, 48                  ; sys_shutdown()
+    pop     rdi                      ; Load socket file descriptor
+    mov     rsi, 2                   ; SHUT_RDWR
     syscall
     ret
